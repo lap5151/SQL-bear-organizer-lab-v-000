f92fca7cae4fff9e061ffb52787fa769e0f186a4
Lauren Pardee <lap5151@gmail.com>
2018-09-26 16:33:20 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -20,6 +20,7 @@ end
 
 def selects_most_prominent_color_and_returns_with_count
   "SELECT MAX(color) AS common_color FROM bears"
+  "SELECT COUNT(color) FROM bears WHERE color = common_color"
 end
 
 def counts_number_of_bears_with_goofy_temperaments
